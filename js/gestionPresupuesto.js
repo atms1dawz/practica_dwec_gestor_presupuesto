02573d7d3a6f22eda64e0d311b51f366c12c2b47
atms1dawz <angmorsen@alu.edu.gva.es>
2024-05-10 19:35:11 +0200

    Corrección de los métodos listarGastos y anyadirGasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -15,13 +15,22 @@ let idGasto = 0; // Ej. 2 - Almacena el identificador actual de cada gasto. Inic
 mostrarPresupuesto() */
 
 // Ej. 2 - Llamada a las funciones de la práctica: Fundamentos de JavaScript II
-let gasto = new CrearGasto("Ejemplo de gasto 1", 20.33, "2023-10-11","ordinario","extraordinario","comunidad","luz","agua");
-console.log(`La fecha del gasto en timestamp es: ${gasto.fecha}`);
-let gastosVarios = "";
-for (let i = 0; i < gasto.etiquetas.length; i++){
-    gastosVarios += `-${gasto.etiquetas[i]}\n`;
+
+let gasto1 = new CrearGasto("Ejemplo de gasto 1", 20.33, "2023-10-11","ordinario","extraordinario","comunidad","luz","agua");
+console.log(`La fecha del gasto en timestamp es: ${gasto1.fecha}`);
+
+console.log(`Los gastos del ejercicio son:`);
+for (let gastosVariados of gasto1.etiquetas){
+    console.log(`-${gastosVariados}`);
 }
-console.log(`Los gastos del ejercicio son:\n ${gastosVarios}`);
+/*
+console.log(`Antes de añadir el gasto ${listarGastos()}`);
+console.log(`Antes de añadir el gasto ${gastos.length}`);
+anyadirGasto(gasto1); 
+console.log(`Después de añadir el gasto ${listarGastos()}`);
+console.log(`Después de añadir el gasto ${gastos.length}`);
+*/
+
 
 
 
@@ -56,11 +65,24 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
             this.descripcion = descripcion;
             this.valor = (valor >=0) ? valor : 0;
 
-
+            // Ej.2 - FECHA
+            // Ej.2 - Si no se indica el parámetro fecha, se almacenará en la propiedad fecha la fecha actual.
+            // Ej.2 - El parámetro fecha deberá ser un string con formato válido que pueda entender la función Date.parse. 
+            // Si la fecha no es válida (no sigue el formato indicado), se deberá almacenar la fecha actual en su lugar.
+            // Ej.2 - Tal como se indica en la sección de objeto gasto, la fecha se almacenará en formato timestamp.
+            
+            // Utilizo un condicional ternario
+            this.fecha = (!isNaN(Date.parse(fecha))) ? new Date(fecha).getTime() : Date.now();
+            // Algunas de las líneas de código probadas sin éxito:
             //this.fecha = (((Date.parse(fecha)).isNaN) == false) ? new Date(fecha) : new Date;
             //this.fecha = (!isNaN(Date.parse(fecha))) ? new Date(fecha) : new Date();
-            this.fecha = (!isNaN(Date.parse(fecha))) ? new Date(fecha).getTime() : Date.now();
-            //this.etiquetas = [];
+
+
+
+
+            // Ej.2 - ETIQUETAS
+            // Si no se indican los parámetros de etiquetas, se almacenará en la propiedad etiquetas un array vacío.
+            // Utilizo un condicional ternario
             this.etiquetas = (etiquetas.length > 0) ? etiquetas : [];
 
 
@@ -87,10 +109,16 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
               }     
               
             // Ej. 2 - Métodos creados en el Ejercicio 2º
-            // Ej. 2 - Crear función xx
-            this.anyadirGasto = function(...lleganTags){
-                    this.etiquetas.push(lleganTags);
-            }
+            // Ej. 2 - Crear función de 1 parámetro que realizará tres tareas:
+            //Añadir al objeto gasto pasado como parámetro una propiedad id cuyo valor será el valor actual de la variable global idGasto.
+            //Incrementar el valor de la variable global idGasto.
+            //Añadir el objeto gasto pasado como parámetro a la variable global gastos. El gasto se debe añadir al final del array.
+
+           /* this.anyadirGasto = function(gasto){
+                this.id = idGasto;
+                idGasto++;
+                gastos.push(gasto);
+            }*/
 
             // Ej. 2 - Crear función xx
             this.borrarGasto = function(id){
@@ -108,11 +136,13 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
 
 // Ej. 2 - Funciones para añadir al final - FUNCIONES de la práctica: Fundamentos de JavaScript II
         function listarGastos(){
-            return gastos;
+            return (gastos.length == 0 ? [] : gastos);
         }
 
-        function anyadirGasto(){
-
+        function anyadirGasto(gasto){
+            gasto.id = idGasto;
+            idGasto++;
+            gastos.push(gasto);
         }
 
         function borrarGasto(){
@@ -132,6 +162,8 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
+
+// En el Ej. 2 se añaden listarGastos, anyadirGasto, borrarGasto, calcularTotalGastos y calcularBalance 
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
